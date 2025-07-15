47a22c386513bfe3324deb1f714b5080d176163b
Ivan Suarez <sua230009@byui.edu>
2025-07-15 17:10:19 -0500

    DOM

--- week2/BookOfMormon/scripts/bom.js
+++ week2/BookOfMormon/scripts/bom.js
@@ -1,15 +1,32 @@
 const input =  document.querySelector("#favchap");
 const button = document.querySelector("button");
-const list = document.querySelector("________________");
+const list = document.querySelector("#list");
 
-const chapters = document.createElement("li");
-const deleteButton = document.createElement("button");
-chapters.textContent = input.value;
-deleteButton.textContent = "X";
-chapters.append(deleteButton);
-list.append(chapters);
 
-let el = document.getElementById("close-button");
-console.log(el.getAttribute("aria-label")); // "Close"
-el.setAttribute("aria-label", "Close dialog");
-console.log(el.getAttribute("aria-label")); // "Close dialog"
+button.addEventListener("click",  function () {
+    if (input.value.trim() === "") {
+        alert("Please enter a chapter.");
+        input.focus();
+        return;
+    }
+
+
+    const chapters = document.createElement("li");
+    const deleteButton = document.createElement("button");
+
+
+    chapters.textContent = input.value;
+    deleteButton.textContent = "❌";
+
+    deleteButton.addEventListener("click", function () {
+    list.removeChild(chapters);
+    input.focus();
+    });
+
+
+    chapters.append(deleteButton);
+    list.append(chapters);
+
+    input.value = "";
+    input.focus();
+});
